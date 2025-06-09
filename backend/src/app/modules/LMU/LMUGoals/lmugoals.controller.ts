2dee0e9fef449f6278196d744255ef61967710d7
unknown <aroarko.sd@gmail.com>
2025-06-10 02:18:17 +0800

    lmu goal creation completed

--- backend/src/app/modules/LMU/LMUGoals/lmugoals.controller.ts
+++ backend/src/app/modules/LMU/LMUGoals/lmugoals.controller.ts
@@ -1,7 +1,20 @@
 import catchAsync from '../../../utils/catchAsync';
+import sendResponse from '../../../utils/sendReponse';
+import httpStatus from 'http-status';
+import { lmuGoalsService } from './lmugoals.service';
 
 const createLmuGoal = catchAsync(async (req, res) => {
   const user = req.user;
+  const data = req.body;
+
+  const result = await lmuGoalsService.createLmuGoal(user, data);
+
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: 'Goal created successfully',
+    data: result,
+  });
 });
 
 export const lmuGoalsController = {
--- backend/src/app/modules/LMU/LMUGoals/lmugoals.service.ts
+++ backend/src/app/modules/LMU/LMUGoals/lmugoals.service.ts
@@ -1,4 +1,21 @@
-const createLmuGoal = async () => {};
+import { JwtPayload } from 'jsonwebtoken';
+import { TGoal } from './lmugoals.interface';
+import { User } from '../../User/user.model';
+import { LMUGoalModel } from './lmugoals.model';
+
+const createLmuGoal = async (currentUser: JwtPayload, data: TGoal) => {
+  const { email } = currentUser;
+  const user = await User.findOne({ email });
+
+  //   assigning the user to the createdBy field
+  if (!user) {
+    throw new Error('Please login to create a goal');
+  }
+  data.createdBy = user._id;
+
+  const result = await LMUGoalModel.create(data);
+  return result;
+};
 
 export const lmuGoalsService = {
   createLmuGoal,
