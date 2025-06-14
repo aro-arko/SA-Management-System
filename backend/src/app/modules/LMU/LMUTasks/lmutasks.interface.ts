36354d9ac8c8c1699034fe2d0d7a55d5f14ef5ef
unknown <aroarko.sd@gmail.com>
2025-06-14 23:02:42 +0800

    LMU tasks module is removed

--- backend/src/app/modules/LMU/LMUTasks/lmutasks.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Types } from 'mongoose';
-
-export type TLMU = {
-  title: string;
-  type: 'whatsapp' | 'email' | 'calling' | 'data-entry' | 'others';
-  unit: string;
-  task: Types.ObjectId;
-};
--- backend/src/app/modules/LMU/LMUTasks/lmutasks.route.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-// router.post('/create-task');
-
-export const LMURoutes = router;
