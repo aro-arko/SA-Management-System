977a17b4792145a38e451fee9e69e99ea3fa961b
unknown <aroarko.sd@gmail.com>
2025-06-24 21:59:44 +0800

    fixedtype event type

---
+++ backend/src/app/modules/EMU/FixedTimeEvent/fixedtimeevent.interface.ts
@@ -0,0 +1,14 @@
+import { Types } from 'mongoose';
+
+export type TFixedTimeEvent = {
+  title: string;
+  multiTask: boolean;
+  multiTaskId: Types.ObjectId;
+  eventDate: Date;
+  createdBy: Types.ObjectId;
+  selectedManpower: Types.ObjectId[];
+  startTime: Date;
+  endTime: Date;
+  status: 'active' | 'inactive';
+  attendanceReport: Types.ObjectId;
+};
