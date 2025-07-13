8ace85871ef5e442727746ed77098b5b9fe421f5
unknown <aroarko.sd@gmail.com>
2025-07-13 20:34:33 +0800

    new application model added

---
+++ backend/src/app/modules/HR & Finance/NewApplications/newapplications.model.ts
@@ -0,0 +1,26 @@
+import { model, Schema } from 'mongoose';
+import { TNewApplication } from './newapplications.interface';
+
+const newApplicationSchema = new Schema<TNewApplication>(
+  {
+    fullName: { type: String, required: true },
+    studentId: { type: Number, required: true },
+    expectedGraduationDate: { type: Date, required: true },
+    email: { type: String, required: true },
+    phoneNumber: { type: Number, required: true },
+    Faculty: { type: String, required: true },
+    Major: { type: String, required: true },
+    ResumeLink: { type: String, required: true },
+    isChecked: { type: Boolean, default: false },
+    checkedBy: { type: Schema.Types.ObjectId, ref: 'User', required: false },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const NewApplication = model<TNewApplication>(
+  'NewApplication',
+  newApplicationSchema,
+);
