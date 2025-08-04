6324ecdf0cca99cd2fac98f20c789f299460a8c0
unknown <aroarko.sd@gmail.com>
2025-08-05 00:36:09 +0800

    getAllDataBatches api implemented

--- frontend/src/services/LMUService/dataManagement/index.ts
+++ frontend/src/services/LMUService/dataManagement/index.ts
@@ -3,11 +3,32 @@
 
 import { cookies } from "next/headers";
 
+// get all data entry tasks with pagination
 export const getAllDataEntryTasks = async (query: string) => {
   const token = (await cookies()).get("accessToken")?.value;
   try {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_BASE_API}/data-management/all-tasks?${query}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${token}`,
+        },
+      }
+    );
+    return res.json();
+  } catch (error: any) {
+    return error;
+  }
+};
+
+// get all data batches
+export const getAllDataBatches = async (query: string) => {
+  const token = (await cookies()).get("accessToken")?.value;
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API}/data-management/batches?${query}`,
       {
         method: "GET",
         headers: {
