bac3da6b88e6ff0b16b1f7680a31358fbcbeb6e3
unknown <aroarko.sd@gmail.com>
2025-08-07 19:14:19 +0800

    fetching users api added

--- frontend/src/services/UserService/index.ts
+++ frontend/src/services/UserService/index.ts
@@ -51,3 +51,28 @@ export const TaskDetails = async (taskId: string) => {
     return error.message || "An error occurred while fetching task details";
   }
 };
+
+export const getAllUsers = async (query: string) => {
+  const token = (await cookies()).get("accessToken")?.value;
+
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API}/users?${query}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${token}`,
+        },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch users");
+    }
+
+    return res.json();
+  } catch (error: any) {
+    return error.message || "An error occurred while fetching users";
+  }
+};
