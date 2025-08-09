8cfd24e0526b37702680c491c08ffaf81e6745c5
unknown <aroarko.sd@gmail.com>
2025-08-09 22:26:44 +0800

    emu mulitaskings and application added for lmu admin

---
+++ frontend/src/app/(WithDashboardLayout)/lmuadmin/emu-multitaskings/[id]/page.tsx
@@ -0,0 +1,11 @@
+import EmuMultitaskingDetails from "@/components/modules/WithDashboardLayout/Event Management Unit/Multitaskings/EmuMultitaskingDetails";
+
+const EMUMultitaskingDetailsLmuAdminPage = () => {
+  return (
+    <div>
+      <EmuMultitaskingDetails />
+    </div>
+  );
+};
+
+export default EMUMultitaskingDetailsLmuAdminPage;
---
+++ frontend/src/app/(WithDashboardLayout)/lmuadmin/emu-multitaskings/page.tsx
@@ -0,0 +1,11 @@
+import EmuMultitaskings from "@/components/modules/WithDashboardLayout/Event Management Unit/Multitaskings/EmuMultitaskings";
+
+const EMUMultitaskingsLmuAdminPage = () => {
+  return (
+    <div>
+      <EmuMultitaskings />
+    </div>
+  );
+};
+
+export default EMUMultitaskingsLmuAdminPage;
