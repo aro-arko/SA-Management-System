2fbb08724513a8eff004ab7784183fbc5af06ee3
unknown <aroarko.sd@gmail.com>
2025-08-09 22:36:31 +0800

    dsmm multitasking added for lmu admin

---
+++ frontend/src/app/(WithDashboardLayout)/lmuadmin/dsmm-multitaskings/[id]/page.tsx
@@ -0,0 +1,11 @@
+import DSMMMultitaskingDetails from "@/components/modules/WithDashboardLayout/DSMM Unit/Multitaskings/DSMMMultitaskingDetails";
+
+const DSMMMultitaskingDetailsLmuAdminPage = () => {
+  return (
+    <div>
+      <DSMMMultitaskingDetails />
+    </div>
+  );
+};
+
+export default DSMMMultitaskingDetailsLmuAdminPage;
---
+++ frontend/src/app/(WithDashboardLayout)/lmuadmin/dsmm-multitaskings/page.tsx
@@ -0,0 +1,11 @@
+import DSMMMultitaskings from "@/components/modules/WithDashboardLayout/DSMM Unit/Multitaskings/DSMMMultitaskings";
+
+const DSMMMultitaskingsLmuAdminPage = () => {
+  return (
+    <div>
+      <DSMMMultitaskings />
+    </div>
+  );
+};
+
+export default DSMMMultitaskingsLmuAdminPage;
