3e6619e720fc03de32579ee8c124376f6889c80d
unknown <aroarko.sd@gmail.com>
2025-08-09 20:55:18 +0800

    change password added for head role

---
+++ frontend/src/app/(WithDashboardLayout)/head/change-password/page.tsx
@@ -0,0 +1,11 @@
+import ChangePassword from "@/components/modules/WithDashboardLayout/Change Password/ChangePassword";
+
+const ChangePasswordHeadPage = () => {
+  return (
+    <div>
+      <ChangePassword />
+    </div>
+  );
+};
+
+export default ChangePasswordHeadPage;
