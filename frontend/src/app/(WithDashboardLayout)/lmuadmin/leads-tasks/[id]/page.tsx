4cf731b623f06e90197b63daa36d657d48423712
unknown <aroarko.sd@gmail.com>
2025-08-09 21:51:39 +0800

    leads tasks details added

---
+++ frontend/src/app/(WithDashboardLayout)/lmuadmin/leads-tasks/[id]/page.tsx
@@ -0,0 +1,11 @@
+import LeadsTaskDetails from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Leads Tasks/LeadsTaskDetails";
+
+const LeadsTaskDetailsLmuAdminPage = () => {
+  return (
+    <div>
+      <LeadsTaskDetails />
+    </div>
+  );
+};
+
+export default LeadsTaskDetailsLmuAdminPage;
