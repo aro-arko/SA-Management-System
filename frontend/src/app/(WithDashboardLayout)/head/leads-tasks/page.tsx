e27b51f06b4f54b3e239343fb002239936155488
unknown <aroarko.sd@gmail.com>
2025-08-09 20:00:06 +0800

    leads tasks added

---
+++ frontend/src/app/(WithDashboardLayout)/head/leads-tasks/page.tsx
@@ -0,0 +1,11 @@
+import LeadsTasks from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Leads Tasks/LeadsTasks";
+
+const LeadsTaskPageHead = () => {
+  return (
+    <div>
+      <LeadsTasks />
+    </div>
+  );
+};
+
+export default LeadsTaskPageHead;
