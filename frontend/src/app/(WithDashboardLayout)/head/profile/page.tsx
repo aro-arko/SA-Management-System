deb2d8f4fcb7e2867aff66f057ca14ce7c912781
unknown <aroarko.sd@gmail.com>
2025-08-09 20:53:53 +0800

    profile page added for head

---
+++ frontend/src/app/(WithDashboardLayout)/head/profile/page.tsx
@@ -0,0 +1,11 @@
+import Profile from "@/components/modules/WithDashboardLayout/Profile/Profile";
+
+const ProfileHeadPage = () => {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+};
+
+export default ProfileHeadPage;
