c59343914381a4170ffcf62397e4c2143e58254d
unknown <aroarko.sd@gmail.com>
2025-08-09 21:21:09 +0800

    lmu admin sidbar added

---
+++ frontend/src/components/role-base-sidebar/lmuadminSidebarNav.ts
@@ -0,0 +1,77 @@
+import { Users, Settings, HomeIcon, Workflow, Clipboard } from "lucide-react";
+
+export const headNavMain = [
+  {
+    title: "Home",
+    url: "/",
+    icon: HomeIcon,
+  },
+  {
+    title: "My Tasks",
+    url: "/head/my-tasks",
+    icon: Clipboard,
+    isActive: true,
+  },
+  {
+    title: "Multitaskings",
+    url: "#",
+    icon: Workflow,
+    items: [
+      {
+        title: "EMU Multitaskings",
+        url: "/head/emu-multitaskings",
+      },
+      {
+        title: "DSMM Multitaskings",
+        url: "/head/dsmm-multitaskings",
+      },
+    ],
+  },
+  {
+    title: "Leads Management Unit",
+    url: "#",
+    icon: Users,
+    items: [
+      {
+        title: "Leads Tasks",
+        url: "/head/leads-tasks",
+      },
+      {
+        title: "Leads Goals",
+        url: "/head/leads-goals",
+      },
+      {
+        title: "Data Entry Tasks",
+        url: "/head/data-entry-tasks",
+      },
+      {
+        title: "Data Batches",
+        url: "/head/data-batches",
+      },
+      {
+        title: "Multitaskings",
+        url: "/head/lmu-multitaskings",
+      },
+      {
+        title: "Others",
+        url: "/head/lmu-others",
+      },
+    ],
+  },
+
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+    items: [
+      {
+        title: "Profile",
+        url: "/head/profile",
+      },
+      {
+        title: "Change Password",
+        url: "/head/change-password",
+      },
+    ],
+  },
+];
