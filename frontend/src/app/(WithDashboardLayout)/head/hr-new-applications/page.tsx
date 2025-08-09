7fc3aecf5b9e8ce0392dad870ce991bf695eff9d
unknown <aroarko.sd@gmail.com>
2025-08-09 20:35:54 +0800

    hr new applications for head role

---
+++ frontend/src/app/(WithDashboardLayout)/head/hr-new-applications/page.tsx
@@ -0,0 +1,11 @@
+import NewApplications from "@/components/modules/WithDashboardLayout/HR_Finance Unit/New Applications/NewApplications";
+
+const HRNewApplicationsHeadPage = () => {
+  return (
+    <div>
+      <NewApplications />
+    </div>
+  );
+};
+
+export default HRNewApplicationsHeadPage;
