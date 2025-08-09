3cb1155b8ae3cea037d8a95ec8765c3684b17d2e
unknown <aroarko.sd@gmail.com>
2025-08-09 22:08:45 +0800

    data entry task details added for lmu admin

---
+++ frontend/src/app/(WithDashboardLayout)/lmuadmin/data-entry-tasks/[id]/page.tsx
@@ -0,0 +1,11 @@
+import DataEntryTaskDetails from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Data Entry Tasks/DataEntryTaskDetails";
+
+const DataEntryTaskDetailsLmuAdminPage = () => {
+  return (
+    <div>
+      <DataEntryTaskDetails />
+    </div>
+  );
+};
+
+export default DataEntryTaskDetailsLmuAdminPage;
