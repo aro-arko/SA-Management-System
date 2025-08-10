5786d26513067c13beda97fc99e550e8383bf0ff
unknown <aroarko.sd@gmail.com>
2025-08-10 23:45:59 +0800

    change password added to lmu data leader role

---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/change-password/page.tsx
@@ -0,0 +1,11 @@
+import ChangePassword from "@/components/modules/WithDashboardLayout/Change Password/ChangePassword";
+
+const ChangePasswordLmuDataLeaderPage = () => {
+  return (
+    <div>
+      <ChangePassword />
+    </div>
+  );
+};
+
+export default ChangePasswordLmuDataLeaderPage;
