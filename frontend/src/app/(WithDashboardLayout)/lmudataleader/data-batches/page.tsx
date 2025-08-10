804fcce0eae6a95622bab4f98a93d69c77fdbe8a
unknown <aroarko.sd@gmail.com>
2025-08-10 23:24:08 +0800

    data batch & details added for lmu data leader role

---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/data-batches/page.tsx
@@ -0,0 +1,11 @@
+import DataBatches from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Data Batches/DataBatches";
+
+const DataBatchesLmuDataLeaderPage = () => {
+  return (
+    <div>
+      <DataBatches />
+    </div>
+  );
+};
+
+export default DataBatchesLmuDataLeaderPage;
