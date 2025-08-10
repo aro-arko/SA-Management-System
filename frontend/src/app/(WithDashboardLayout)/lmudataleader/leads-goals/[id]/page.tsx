07f7ac6d5b332c6609ea5d36071e886637de4e4e
unknown <aroarko.sd@gmail.com>
2025-08-10 23:12:54 +0800

    lmu leads goals and details added for data leader role

---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/leads-goals/[id]/page.tsx
@@ -0,0 +1,11 @@
+import LeadsGoalDetails from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Leads Goals/LeadsGoalDetails";
+
+const LeadsGoalDetailsLmuDataLeader = () => {
+  return (
+    <div>
+      <LeadsGoalDetails />
+    </div>
+  );
+};
+
+export default LeadsGoalDetailsLmuDataLeader;
---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/leads-goals/page.tsx
@@ -0,0 +1,11 @@
+import LeadsGoals from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Leads Goals/LeadsGoals";
+
+const LeadsGoalsLmuDataLeaderPage = () => {
+  return (
+    <div>
+      <LeadsGoals />
+    </div>
+  );
+};
+
+export default LeadsGoalsLmuDataLeaderPage;
