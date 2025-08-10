077195dca9fd1959e001182c3596084893c46da0
unknown <aroarko.sd@gmail.com>
2025-08-10 23:35:34 +0800

    create multitasking privilege added for lmu data leader

---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/create-multitasking/page.tsx
@@ -0,0 +1,11 @@
+import CreateMultitasking from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Multitaskings/CreateMultitasking";
+
+const CreateMultitaskingLmuDataLeaderPage = () => {
+  return (
+    <div>
+      <CreateMultitasking />
+    </div>
+  );
+};
+
+export default CreateMultitaskingLmuDataLeaderPage;
