2e273710ec292416e71f355864e6e9ec1aaf8243
unknown <aroarko.sd@gmail.com>
2025-08-10 23:47:58 +0800

    lmu member tasks added

---
+++ frontend/src/app/(WithDashboardLayout)/lmumember/my-tasks/page.tsx
@@ -0,0 +1,11 @@
+import MyTasks from "@/components/modules/WithDashboardLayout/My Tasks/MyTasks";
+
+const MyTaskLmuMemberPage = () => {
+  return (
+    <div>
+      <MyTasks />
+    </div>
+  );
+};
+
+export default MyTaskLmuMemberPage;
