44efc5a63caaf760f8d5381865c856760b682d6f
unknown <aroarko.sd@gmail.com>
2025-08-10 23:01:18 +0800

    emu multitasking with details added for lmu data leader role

---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/emu-multitaskings/page.tsx
@@ -0,0 +1,11 @@
+import EmuMultitaskings from "@/components/modules/WithDashboardLayout/Event Management Unit/Multitaskings/EmuMultitaskings";
+
+const EmuMultitaskingLmuDataLeaderPage = () => {
+  return (
+    <div>
+      <EmuMultitaskings />
+    </div>
+  );
+};
+
+export default EmuMultitaskingLmuDataLeaderPage;
