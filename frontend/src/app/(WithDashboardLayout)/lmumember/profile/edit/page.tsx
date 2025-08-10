1391d6bc71a1e65be8d8f3ea8d3e877b9a89da64
unknown <aroarko.sd@gmail.com>
2025-08-11 02:26:13 +0800

    profile section added for lmu member

---
+++ frontend/src/app/(WithDashboardLayout)/lmumember/profile/edit/page.tsx
@@ -0,0 +1,11 @@
+import EditOwnProfile from "@/components/modules/WithDashboardLayout/Profile/EditOwnProfile";
+
+const EditProfileLmuMemberPage = () => {
+  return (
+    <div>
+      <EditOwnProfile />
+    </div>
+  );
+};
+
+export default EditProfileLmuMemberPage;
