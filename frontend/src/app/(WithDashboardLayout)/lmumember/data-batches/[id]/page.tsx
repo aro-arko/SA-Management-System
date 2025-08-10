56176558d52ab6f46b2ff39d442d32b4619da858
unknown <aroarko.sd@gmail.com>
2025-08-11 02:14:25 +0800

    data batch with details added for lmu member

---
+++ frontend/src/app/(WithDashboardLayout)/lmumember/data-batches/[id]/page.tsx
@@ -0,0 +1,11 @@
+import DataBatchDetails from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Data Batches/DataBatchDetails";
+
+const DataBatchDetailsLmeMemberPage = () => {
+  return (
+    <div>
+      <DataBatchDetails />
+    </div>
+  );
+};
+
+export default DataBatchDetailsLmeMemberPage;
