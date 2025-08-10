a5a05590fd41a33455415ffee453bf0d7acfee94
unknown <aroarko.sd@gmail.com>
2025-08-10 16:03:35 +0800

    lmu multiaskings for lmu admin added

---
+++ frontend/src/app/(WithDashboardLayout)/lmuadmin/lmu-multitaskings/page.tsx
@@ -0,0 +1,11 @@
+import Multitaskings from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Multitaskings/Multitaskings";
+
+const LmuMultitaskingLmuAdminPage = () => {
+  return (
+    <div>
+      <Multitaskings />
+    </div>
+  );
+};
+
+export default LmuMultitaskingLmuAdminPage;
