cda676bde723071702668c381c58a102d48770f7
unknown <aroarko.sd@gmail.com>
2025-08-10 23:03:22 +0800

    dsmm multitasking details added for lmu data leader role

---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/dsmm-multitaskings/[id]/page.tsx
@@ -0,0 +1,11 @@
+import DSMMMultitaskingDetails from "@/components/modules/WithDashboardLayout/DSMM Unit/Multitaskings/DSMMMultitaskingDetails";
+
+const DSMMMultitaskingDetailsLmuDataLeaderPage = () => {
+  return (
+    <div>
+      <DSMMMultitaskingDetails />
+    </div>
+  );
+};
+
+export default DSMMMultitaskingDetailsLmuDataLeaderPage;
---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/dsmm-multitaskings/page.tsx
@@ -0,0 +1,11 @@
+import DSMMMultitaskings from "@/components/modules/WithDashboardLayout/DSMM Unit/Multitaskings/DSMMMultitaskings";
+
+const DSMMMultitaskingsLmuDataLeaderPage = () => {
+  return (
+    <div>
+      <DSMMMultitaskings />
+    </div>
+  );
+};
+
+export default DSMMMultitaskingsLmuDataLeaderPage;
