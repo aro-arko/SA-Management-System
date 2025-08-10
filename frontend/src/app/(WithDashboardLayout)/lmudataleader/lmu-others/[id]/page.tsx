98db4a21b207039d9ec2ebb8fa57c3dd8ba6b2af
unknown <aroarko.sd@gmail.com>
2025-08-10 23:39:40 +0800

    lmu others task with details added for lmu data leader

---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/lmu-others/[id]/page.tsx
@@ -0,0 +1,11 @@
+import LmuOthersTaskDetails from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Others Tasks/LmuOthersTaskDetails";
+
+const LmuOthersTaskDetailsLmuDataLeaderPage = () => {
+  return (
+    <div>
+      <LmuOthersTaskDetails />
+    </div>
+  );
+};
+
+export default LmuOthersTaskDetailsLmuDataLeaderPage;
---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/lmu-others/page.tsx
@@ -0,0 +1,11 @@
+import LmuOthersTasks from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Others Tasks/LmuOthersTasks";
+
+const page = () => {
+  return (
+    <div>
+      <LmuOthersTasks />
+    </div>
+  );
+};
+
+export default page;
