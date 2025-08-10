c427ec2042f61c468e5d0e9233eb6a6d507f0ab4
unknown <aroarko.sd@gmail.com>
2025-08-11 01:54:47 +0800

    emu multitasking with details page added for lmu member role

---
+++ frontend/src/app/(WithDashboardLayout)/lmumember/emu-multitaskings/page.tsx
@@ -0,0 +1,11 @@
+import EmuMultitaskings from "@/components/modules/WithDashboardLayout/Event Management Unit/Multitaskings/EmuMultitaskings";
+
+const EMUMultitaskingsLmuMemberPage = () => {
+  return (
+    <div>
+      <EmuMultitaskings />
+    </div>
+  );
+};
+
+export default EMUMultitaskingsLmuMemberPage;
