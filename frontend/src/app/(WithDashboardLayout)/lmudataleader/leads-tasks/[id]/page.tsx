9392334a609c9f86bc23e477fae7afa656415d83
unknown <aroarko.sd@gmail.com>
2025-08-10 23:10:12 +0800

    leads task details added for lmu data leader role

---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/leads-tasks/[id]/page.tsx
@@ -0,0 +1,11 @@
+import LeadsTaskDetails from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Leads Tasks/LeadsTaskDetails";
+
+const LeadsTaskDetailsLmuDataLeaderPage = () => {
+  return (
+    <div>
+      <LeadsTaskDetails />
+    </div>
+  );
+};
+
+export default LeadsTaskDetailsLmuDataLeaderPage;
---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/leads-tasks/page.tsx
@@ -0,0 +1,11 @@
+import LeadsTasks from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Leads Tasks/LeadsTasks";
+
+const LeadsTasksLmuDataLeaderPage = () => {
+  return (
+    <div>
+      <LeadsTasks />
+    </div>
+  );
+};
+
+export default LeadsTasksLmuDataLeaderPage;
