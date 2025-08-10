a04e1b2fbfb8f62ec35be22d6b9825f158d09325
unknown <aroarko.sd@gmail.com>
2025-08-10 23:21:10 +0800

    create data entry task added for lmu data leader

---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/create-data-entry-task/page.tsx
@@ -0,0 +1,11 @@
+import CreateDataEntryTask from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Data Entry Tasks/CreateDataEntryTask";
+
+const CreateDataEntryTaskLmuDataLeaderPage = () => {
+  return (
+    <div>
+      <CreateDataEntryTask />
+    </div>
+  );
+};
+
+export default CreateDataEntryTaskLmuDataLeaderPage;
