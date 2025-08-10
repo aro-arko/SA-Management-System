2750ec87be56d5821af6663a730b2c8eaace4f33
unknown <aroarko.sd@gmail.com>
2025-08-10 23:25:55 +0800

    lmu data batch creation added for lmu data leader

---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/create-data-batch/page.tsx
@@ -0,0 +1,11 @@
+import CreateDataBatch from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Data Batches/CreateDataBatch";
+
+const CreateDataBatchLmuDataLeaderPage = () => {
+  return (
+    <div>
+      <CreateDataBatch />
+    </div>
+  );
+};
+
+export default CreateDataBatchLmuDataLeaderPage;
