1ef159b1870544c687b76b613dc8a132e555a6d4
unknown <aroarko.sd@gmail.com>
2025-08-10 22:58:16 +0800

    mytasks page added for lmu data leader role

---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/my-tasks/page.tsx
@@ -0,0 +1,11 @@
+import MyTasks from "@/components/modules/WithDashboardLayout/My Tasks/MyTasks";
+
+const MyTasksLmuDataLeaderPage = () => {
+  return (
+    <div>
+      <MyTasks />
+    </div>
+  );
+};
+
+export default MyTasksLmuDataLeaderPage;
