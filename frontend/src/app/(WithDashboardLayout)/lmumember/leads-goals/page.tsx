c81fb6e6dc16ebd95289dad0db71f67e902ddca8
unknown <aroarko.sd@gmail.com>
2025-08-11 02:07:27 +0800

    leads goals section added for lmu member role

---
+++ frontend/src/app/(WithDashboardLayout)/lmumember/leads-goals/page.tsx
@@ -0,0 +1,11 @@
+import LeadsGoals from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Leads Goals/LeadsGoals";
+
+const LeadsGoalsLmuMemberPage = () => {
+  return (
+    <div>
+      <LeadsGoals />
+    </div>
+  );
+};
+
+export default LeadsGoalsLmuMemberPage;
