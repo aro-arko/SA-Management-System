bd94651f9f7d1f3426b33aaa1018bc2ea0bc3017
unknown <aroarko.sd@gmail.com>
2025-08-11 13:54:45 +0800

    dsmm multitaskings with details added for emu admin

---
+++ frontend/src/app/(WithDashboardLayout)/emuadmin/dsmm-multitaskings/[id]/page.tsx
@@ -0,0 +1,11 @@
+import DSMMMultitaskingDetails from "@/components/modules/WithDashboardLayout/DSMM Unit/Multitaskings/DSMMMultitaskingDetails";
+
+const DSMMMultitaskingDetailsEmuAdminPage = () => {
+  return (
+    <div>
+      <DSMMMultitaskingDetails />
+    </div>
+  );
+};
+
+export default DSMMMultitaskingDetailsEmuAdminPage;
