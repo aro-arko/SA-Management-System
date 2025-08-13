fcb81e66d59d50650c7fbdeb0dd606167eb1d16b
unknown <aroarko.sd@gmail.com>
2025-08-14 03:55:04 +0800

    my tasks added for hr finance admin role

---
+++ frontend/src/app/(WithDashboardLayout)/hrfinanceadmin/my-tasks/page.tsx
@@ -0,0 +1,11 @@
+import MyTasks from "@/components/modules/WithDashboardLayout/My Tasks/MyTasks";
+
+const MyTasksHrFinanceAdminPage = () => {
+  return (
+    <div>
+      <MyTasks />
+    </div>
+  );
+};
+
+export default MyTasksHrFinanceAdminPage;
