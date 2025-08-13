a31d712fd7ad4d0f611f0120dc90afba37c96325
unknown <aroarko.sd@gmail.com>
2025-08-13 19:04:29 +0800

    profile page added for emu member role

---
+++ frontend/src/app/(WithDashboardLayout)/emumember/profile/edit/page.tsx
@@ -0,0 +1,11 @@
+import EditOwnProfile from "@/components/modules/WithDashboardLayout/Profile/EditOwnProfile";
+
+const UpdateProfileEmuMember = () => {
+  return (
+    <div>
+      <EditOwnProfile />
+    </div>
+  );
+};
+
+export default UpdateProfileEmuMember;
---
+++ frontend/src/app/(WithDashboardLayout)/emumember/profile/page.tsx
@@ -0,0 +1,11 @@
+import Profile from "@/components/modules/WithDashboardLayout/Profile/Profile";
+
+const ProfileEmuMemberPage = () => {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+};
+
+export default ProfileEmuMemberPage;
