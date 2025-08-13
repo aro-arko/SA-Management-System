7919b9ab34b235bac285ee8cc624f5eb7ec8f8c1
unknown <aroarko.sd@gmail.com>
2025-08-13 23:24:30 +0800

    dsmm tasks details added for lmu member role

---
+++ frontend/src/app/(WithDashboardLayout)/lmumember/dsmm-tasks/[id]/page.tsx
@@ -0,0 +1,11 @@
+import DSMMTaskDetails from "@/components/modules/WithDashboardLayout/DSMM Unit/DSMM Tasks/DSMMTaskDetails";
+
+const DsmmTaskDetailsLmuMemberPage = () => {
+  return (
+    <div>
+      <DSMMTaskDetails />
+    </div>
+  );
+};
+
+export default DsmmTaskDetailsLmuMemberPage;
