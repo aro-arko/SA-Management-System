7bd4ccbd7159a0adbf1e38e460c565c5bb6240a6
unknown <aroarko.sd@gmail.com>
2025-08-14 03:43:14 +0800

    change password added for dsmm admin role

---
+++ frontend/src/app/(WithDashboardLayout)/dsmmadmin/change-password/page.tsx
@@ -0,0 +1,11 @@
+import ChangePassword from "@/components/modules/WithDashboardLayout/Change Password/ChangePassword";
+
+const ChangePasswordDsmmAdminPage = () => {
+  return (
+    <div>
+      <ChangePassword />
+    </div>
+  );
+};
+
+export default ChangePasswordDsmmAdminPage;
