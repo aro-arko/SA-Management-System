491ad8783bfe684acf18646c8ecabb42751924a8
unknown <aroarko.sd@gmail.com>
2025-08-13 23:05:56 +0800

    event task details added for dsmm admin role

---
+++ frontend/src/app/(WithDashboardLayout)/dsmmadmin/event-tasks/[id]/page.tsx
@@ -0,0 +1,11 @@
+import EventTaskDetails from "@/components/modules/WithDashboardLayout/Event Management Unit/Event Tasks/EventTaskDetails";
+
+const EventTaskDetailsDsmmAdminPage = () => {
+  return (
+    <div>
+      <EventTaskDetails />
+    </div>
+  );
+};
+
+export default EventTaskDetailsDsmmAdminPage;
