29cdfd58595125c123a37f68706e80254f3d0720
unknown <aroarko.sd@gmail.com>
2025-08-14 03:59:44 +0800

    emu multitaskings added for hr finance admin

---
+++ frontend/src/app/(WithDashboardLayout)/hrfinanceadmin/emu-multitaskings/page.tsx
@@ -0,0 +1,11 @@
+import EmuMultitaskings from "@/components/modules/WithDashboardLayout/Event Management Unit/Multitaskings/EmuMultitaskings";
+
+const EmuMultitaskingsHrFinanceAdmin = () => {
+  return (
+    <div>
+      <EmuMultitaskings />
+    </div>
+  );
+};
+
+export default EmuMultitaskingsHrFinanceAdmin;
