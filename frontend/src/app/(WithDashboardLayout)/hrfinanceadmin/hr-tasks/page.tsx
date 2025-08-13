445c6de8d88b6c871d95760135267b48df38e222
unknown <aroarko.sd@gmail.com>
2025-08-14 04:07:21 +0800

    hr tasks added for  hr finance admin

---
+++ frontend/src/app/(WithDashboardLayout)/hrfinanceadmin/hr-tasks/page.tsx
@@ -0,0 +1,11 @@
+import HrTasks from "@/components/modules/WithDashboardLayout/HR_Finance Unit/HR Tasks/HrTasks";
+
+const HrFinanceAdminTasksPage = () => {
+  return (
+    <div>
+      <HrTasks />
+    </div>
+  );
+};
+
+export default HrFinanceAdminTasksPage;
