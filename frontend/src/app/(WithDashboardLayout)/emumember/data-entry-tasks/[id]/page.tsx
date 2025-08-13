e827f762bd3f004c55f091302386dd86bbf8d816
unknown <aroarko.sd@gmail.com>
2025-08-13 23:04:04 +0800

    data entry task details added for emu member role

---
+++ frontend/src/app/(WithDashboardLayout)/emumember/data-entry-tasks/[id]/page.tsx
@@ -0,0 +1,11 @@
+import DataEntryTaskDetails from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Data Entry Tasks/DataEntryTaskDetails";
+
+const DataEntryTaskDetailsEmuMemberPage = () => {
+  return (
+    <div>
+      <DataEntryTaskDetails />
+    </div>
+  );
+};
+
+export default DataEntryTaskDetailsEmuMemberPage;
