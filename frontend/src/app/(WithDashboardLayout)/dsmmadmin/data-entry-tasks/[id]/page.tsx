18cd3fd5c28f52b813318c8fdd411f7310f41f7d
unknown <aroarko.sd@gmail.com>
2025-08-13 23:02:17 +0800

    data entry task details added for dsmm admin role

---
+++ frontend/src/app/(WithDashboardLayout)/dsmmadmin/data-entry-tasks/[id]/page.tsx
@@ -0,0 +1,11 @@
+import DataEntryTaskDetails from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Data Entry Tasks/DataEntryTaskDetails";
+
+const DataEntryTaskDetailsDsmmAdminPage = () => {
+  return (
+    <div>
+      <DataEntryTaskDetails />
+    </div>
+  );
+};
+
+export default DataEntryTaskDetailsDsmmAdminPage;
