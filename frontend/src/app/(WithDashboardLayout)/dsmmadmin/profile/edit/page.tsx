cf05aa9c035e37d2d3a64688e9395b88628e34dd
unknown <aroarko.sd@gmail.com>
2025-08-14 03:39:22 +0800

    show and update own profile

---
+++ frontend/src/app/(WithDashboardLayout)/dsmmadmin/profile/edit/page.tsx
@@ -0,0 +1,11 @@
+import EditOwnProfile from "@/components/modules/WithDashboardLayout/Profile/EditOwnProfile";
+
+const UpdateProfileDsmmAdminPage = () => {
+  return (
+    <div>
+      <EditOwnProfile />
+    </div>
+  );
+};
+
+export default UpdateProfileDsmmAdminPage;
---
+++ frontend/src/app/(WithDashboardLayout)/dsmmadmin/profile/page.tsx
@@ -0,0 +1,11 @@
+import Profile from "@/components/modules/WithDashboardLayout/Profile/Profile";
+
+const ProfileDsmmAdminPage = () => {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+};
+
+export default ProfileDsmmAdminPage;
