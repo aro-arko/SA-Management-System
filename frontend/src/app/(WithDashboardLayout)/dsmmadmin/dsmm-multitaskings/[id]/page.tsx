2c0aa0d3690f6660c79eb53cd885d75758a6b6f0
unknown <aroarko.sd@gmail.com>
2025-08-13 23:26:59 +0800

    dsmm multitasking details added for dsmm admin role

---
+++ frontend/src/app/(WithDashboardLayout)/dsmmadmin/dsmm-multitaskings/[id]/page.tsx
@@ -0,0 +1,11 @@
+import DSMMMultitaskingDetails from "@/components/modules/WithDashboardLayout/DSMM Unit/Multitaskings/DSMMMultitaskingDetails";
+
+const DsmmMultitaskingDetailsDsmmAdminPage = () => {
+  return (
+    <div>
+      <DSMMMultitaskingDetails />
+    </div>
+  );
+};
+
+export default DsmmMultitaskingDetailsDsmmAdminPage;
