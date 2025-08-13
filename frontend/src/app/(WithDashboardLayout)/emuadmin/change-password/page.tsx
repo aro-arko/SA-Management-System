e1737b0dc437d4af12a28815a368ab41c51c9b1c
unknown <aroarko.sd@gmail.com>
2025-08-13 18:16:38 +0800

    change password added for emu admin

---
+++ frontend/src/app/(WithDashboardLayout)/emuadmin/change-password/page.tsx
@@ -0,0 +1,11 @@
+import ChangePassword from "@/components/modules/WithDashboardLayout/Change Password/ChangePassword";
+
+const ChangePasswordEmuAdminPage = () => {
+  return (
+    <div>
+      <ChangePassword />
+    </div>
+  );
+};
+
+export default ChangePasswordEmuAdminPage;
