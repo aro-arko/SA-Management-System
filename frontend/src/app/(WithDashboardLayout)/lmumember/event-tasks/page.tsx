81c39113e17b1ba5ccf45e7159064c8f8b91b17d
unknown <aroarko.sd@gmail.com>
2025-08-13 23:08:40 +0800

    event task details added for lmu member role

---
+++ frontend/src/app/(WithDashboardLayout)/lmumember/event-tasks/page.tsx
@@ -0,0 +1,11 @@
+import EventTaskDetails from "@/components/modules/WithDashboardLayout/Event Management Unit/Event Tasks/EventTaskDetails";
+
+const EventTaskDetailsLmuMemberPage = () => {
+  return (
+    <div>
+      <EventTaskDetails />
+    </div>
+  );
+};
+
+export default EventTaskDetailsLmuMemberPage;
