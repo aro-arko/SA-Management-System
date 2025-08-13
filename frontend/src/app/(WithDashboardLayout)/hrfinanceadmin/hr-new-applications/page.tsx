b344c691e06409717ee14b45d3846db08a166f85
unknown <aroarko.sd@gmail.com>
2025-08-14 05:05:51 +0800

    new applications view for hr finance admin

---
+++ frontend/src/app/(WithDashboardLayout)/hrfinanceadmin/hr-new-applications/page.tsx
@@ -0,0 +1,11 @@
+import NewApplications from "@/components/modules/WithDashboardLayout/HR_Finance Unit/New Applications/NewApplications";
+
+const HrNewApplicationsPage = () => {
+  return (
+    <div>
+      <NewApplications />
+    </div>
+  );
+};
+
+export default HrNewApplicationsPage;
