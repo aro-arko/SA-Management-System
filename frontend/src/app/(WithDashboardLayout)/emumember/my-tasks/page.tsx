c2594e05d8f056c577a067692e258d63b45fd6b4
unknown <aroarko.sd@gmail.com>
2025-08-13 18:17:45 +0800

    my tasks added for emu member

---
+++ frontend/src/app/(WithDashboardLayout)/emumember/my-tasks/page.tsx
@@ -0,0 +1,11 @@
+import MyTasks from "@/components/modules/WithDashboardLayout/My Tasks/MyTasks";
+
+const MyTasksEmuMemberPage = () => {
+  return (
+    <div>
+      <MyTasks />
+    </div>
+  );
+};
+
+export default MyTasksEmuMemberPage;
