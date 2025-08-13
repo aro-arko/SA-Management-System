d89882ecad8edd07d2946093216f7d660673755a
unknown <aroarko.sd@gmail.com>
2025-08-13 22:59:47 +0800

    leads task details dsmm admin role

---
+++ frontend/src/app/(WithDashboardLayout)/dsmmadmin/leads-tasks/[id]/page.tsx
@@ -0,0 +1,11 @@
+import LeadsTaskDetails from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Leads Tasks/LeadsTaskDetails";
+
+const LeadsTaskDetailsDsmmAdminPage = () => {
+  return (
+    <div>
+      <LeadsTaskDetails />
+    </div>
+  );
+};
+
+export default LeadsTaskDetailsDsmmAdminPage;
