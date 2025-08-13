81d5c9ba74e8d93c10a3062b880fd43d08e2da3f
unknown <aroarko.sd@gmail.com>
2025-08-13 19:01:38 +0800

    emu multitasking with it's details added for emu member

---
+++ frontend/src/app/(WithDashboardLayout)/emumember/emu-multitaskings/page.tsx
@@ -0,0 +1,11 @@
+import EmuMultitaskings from "@/components/modules/WithDashboardLayout/Event Management Unit/Multitaskings/EmuMultitaskings";
+
+const EmuMultitaskingsEmuMemberPage = () => {
+  return (
+    <div>
+      <EmuMultitaskings />
+    </div>
+  );
+};
+
+export default EmuMultitaskingsEmuMemberPage;
