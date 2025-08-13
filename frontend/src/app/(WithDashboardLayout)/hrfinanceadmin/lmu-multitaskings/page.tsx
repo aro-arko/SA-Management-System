c3d47beb962b202c67cd5ae04c3d91890921b0c7
unknown <aroarko.sd@gmail.com>
2025-08-14 03:57:34 +0800

    lmu multitaskings and details added for hr finance role

---
+++ frontend/src/app/(WithDashboardLayout)/hrfinanceadmin/lmu-multitaskings/page.tsx
@@ -0,0 +1,11 @@
+import Multitaskings from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Multitaskings/Multitaskings";
+
+const LmuMultitaskingsHrFinanceAdminPage = () => {
+  return (
+    <div>
+      <Multitaskings />
+    </div>
+  );
+};
+
+export default LmuMultitaskingsHrFinanceAdminPage;
