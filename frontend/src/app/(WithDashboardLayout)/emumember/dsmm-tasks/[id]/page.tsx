26947e986e9127673934262c57cf67058e9334d4
unknown <aroarko.sd@gmail.com>
2025-08-13 23:18:08 +0800

    dsmm task details added for emu member role

---
+++ frontend/src/app/(WithDashboardLayout)/emumember/dsmm-tasks/[id]/page.tsx
@@ -0,0 +1,11 @@
+import DSMMTaskDetails from "@/components/modules/WithDashboardLayout/DSMM Unit/DSMM Tasks/DSMMTaskDetails";
+
+const DsmmTaskDetailsEmuMemberPage = () => {
+  return (
+    <div>
+      <DSMMTaskDetails />
+    </div>
+  );
+};
+
+export default DsmmTaskDetailsEmuMemberPage;
