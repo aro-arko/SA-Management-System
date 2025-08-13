1c15bd2da4f0e7339db968878cf0b3f1a94e90de
unknown <aroarko.sd@gmail.com>
2025-08-13 19:06:10 +0800

    change password page added for emu member

---
+++ frontend/src/app/(WithDashboardLayout)/emumember/change-password/page.tsx
@@ -0,0 +1,11 @@
+import ChangePassword from "@/components/modules/WithDashboardLayout/Change Password/ChangePassword";
+
+const ChangePasswordEmuMemberPage = () => {
+  return (
+    <div>
+      <ChangePassword />
+    </div>
+  );
+};
+
+export default ChangePasswordEmuMemberPage;
