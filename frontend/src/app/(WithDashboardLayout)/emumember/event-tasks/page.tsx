64b7f6d9dbb10d63dca3cd11e8ae28345a117725
unknown <aroarko.sd@gmail.com>
2025-08-13 18:53:50 +0800

    emu tasks and task details added emu member

---
+++ frontend/src/app/(WithDashboardLayout)/emumember/event-tasks/page.tsx
@@ -0,0 +1,11 @@
+import EventTasks from "@/components/modules/WithDashboardLayout/Event Management Unit/Event Tasks/EventTasks";
+
+const EventTasksEmuMemberPage = () => {
+  return (
+    <div>
+      <EventTasks />
+    </div>
+  );
+};
+
+export default EventTasksEmuMemberPage;
