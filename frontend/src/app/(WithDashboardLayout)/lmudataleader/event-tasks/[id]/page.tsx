f2c0dbcf4d5f07201df9ec3fb854e7eafc4ce196
unknown <aroarko.sd@gmail.com>
2025-08-13 23:07:44 +0800

    event task details for lmu data leader role

---
+++ frontend/src/app/(WithDashboardLayout)/lmudataleader/event-tasks/[id]/page.tsx
@@ -0,0 +1,11 @@
+import EventTaskDetails from "@/components/modules/WithDashboardLayout/Event Management Unit/Event Tasks/EventTaskDetails";
+
+const EventTaskDetailsLmuDataLeaderPage = () => {
+  return (
+    <div>
+      <EventTaskDetails />
+    </div>
+  );
+};
+
+export default EventTaskDetailsLmuDataLeaderPage;
