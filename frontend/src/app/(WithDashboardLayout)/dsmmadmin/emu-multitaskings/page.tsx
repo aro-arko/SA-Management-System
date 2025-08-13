343f91a5fd0b72fb25db579f7069cf66c4574179
unknown <aroarko.sd@gmail.com>
2025-08-13 22:56:15 +0800

    emu multitaskings added dsmm admin

---
+++ frontend/src/app/(WithDashboardLayout)/dsmmadmin/emu-multitaskings/page.tsx
@@ -0,0 +1,11 @@
+import EmuMultitaskings from "@/components/modules/WithDashboardLayout/Event Management Unit/Multitaskings/EmuMultitaskings";
+
+const EmuMultitaskingsDsmmAdminPage = () => {
+  return (
+    <div>
+      <EmuMultitaskings />
+    </div>
+  );
+};
+
+export default EmuMultitaskingsDsmmAdminPage;
