a757c58a85bc840c7a1f0036749b472e7a66b9b0
unknown <aroarko.sd@gmail.com>
2025-08-14 16:25:11 +0800

    change password added for hr finance admin

---
+++ frontend/src/app/(WithDashboardLayout)/hrfinanceadmin/change-password/page.tsx
@@ -0,0 +1,11 @@
+import ChangePassword from "@/components/modules/WithDashboardLayout/Change Password/ChangePassword";
+
+const ChangePasswordHrFinanceAdminPage = () => {
+  return (
+    <div>
+      <ChangePassword />
+    </div>
+  );
+};
+
+export default ChangePasswordHrFinanceAdminPage;
