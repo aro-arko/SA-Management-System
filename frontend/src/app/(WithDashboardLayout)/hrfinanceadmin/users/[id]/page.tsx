d35707b6c9281dc765368975ef8f80272a9ed9f7
unknown <aroarko.sd@gmail.com>
2025-08-14 16:17:46 +0800

    user part added for hrfinance admin

---
+++ frontend/src/app/(WithDashboardLayout)/hrfinanceadmin/users/[id]/page.tsx
@@ -0,0 +1,11 @@
+import UserDetails from "@/components/modules/WithDashboardLayout/Users/UserDetails";
+
+const UserDetailsHrFinanceAdmin = () => {
+  return (
+    <div>
+      <UserDetails />
+    </div>
+  );
+};
+
+export default UserDetailsHrFinanceAdmin;
