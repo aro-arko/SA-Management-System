efb50193c2e739d9c8fa49570b9e94a2a337cba1
unknown <aroarko.sd@gmail.com>
2025-08-14 16:39:36 +0800

    event task details for hr finance admin role

---
+++ frontend/src/app/(WithDashboardLayout)/hrfinanceadmin/event-tasks/[id]/page.tsx
@@ -0,0 +1,11 @@
+import EventTaskDetails from "@/components/modules/WithDashboardLayout/Event Management Unit/Event Tasks/EventTaskDetails";
+
+const EventTaskDetailsHrFinanceAdminPage = () => {
+  return (
+    <div>
+      <EventTaskDetails />
+    </div>
+  );
+};
+
+export default EventTaskDetailsHrFinanceAdminPage;
