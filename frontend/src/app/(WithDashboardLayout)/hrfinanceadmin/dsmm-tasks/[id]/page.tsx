74b1fedbe25dd7cac9c77177da532eb41a20c1dd
unknown <aroarko.sd@gmail.com>
2025-08-14 16:38:40 +0800

    dsmm task details added for hr finance admin role

---
+++ frontend/src/app/(WithDashboardLayout)/hrfinanceadmin/dsmm-tasks/[id]/page.tsx
@@ -0,0 +1,11 @@
+import DSMMTaskDetails from "@/components/modules/WithDashboardLayout/DSMM Unit/DSMM Tasks/DSMMTaskDetails";
+
+const DsmmTaskDetailsHrFinanceAdminPage = () => {
+  return (
+    <div>
+      <DSMMTaskDetails />
+    </div>
+  );
+};
+
+export default DsmmTaskDetailsHrFinanceAdminPage;
