fd5af5c5ed6a1274ee464fe511e38e524b2ebc63
unknown <aroarko.sd@gmail.com>
2025-08-14 16:20:54 +0800

    edit user page added for hrfinance admin

---
+++ frontend/src/app/(WithDashboardLayout)/hrfinanceadmin/users/[id]/update/page.tsx
@@ -0,0 +1,11 @@
+import EditUser from "@/components/modules/WithDashboardLayout/Users/EditUser";
+
+const UpdateUserProfileHrFinanceAdmin = () => {
+  return (
+    <div>
+      <EditUser />
+    </div>
+  );
+};
+
+export default UpdateUserProfileHrFinanceAdmin;
