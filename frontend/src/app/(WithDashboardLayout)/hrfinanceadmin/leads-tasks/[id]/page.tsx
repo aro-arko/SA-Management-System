5c579bfaf2aa415ac5cd3bbbc2d07970e41cf9db
unknown <aroarko.sd@gmail.com>
2025-08-14 16:40:36 +0800

    leads task details added for hr finance admin role

---
+++ frontend/src/app/(WithDashboardLayout)/hrfinanceadmin/leads-tasks/[id]/page.tsx
@@ -0,0 +1,11 @@
+import LeadsTaskDetails from "@/components/modules/WithDashboardLayout/Leads Managment Unit/Leads Tasks/LeadsTaskDetails";
+
+const LeadsTaskDetailsHrFinancePage = () => {
+  return (
+    <div>
+      <LeadsTaskDetails />
+    </div>
+  );
+};
+
+export default LeadsTaskDetailsHrFinancePage;
